2643dae97b7085cf448c78c27eb1c7ae21858d48
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-05-15 15:16:36 +0200

    Authenticate admin for Categories.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,10 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
     end
-
+    
+    def authenticate_admin
+      unless current_user.admin?
+        redirect_to new_user_session_path, error: 'You have to login in order to do that.'
+      end
+    end
 end
