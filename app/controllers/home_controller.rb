6f1778f470a4ed1f09df5b314a04e7dec0bb6638
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-15 10:27:14 +0200

    JS changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   def index
     @category = Category.first
     @product = @category.products.first
+    
+    @count_categories = Category.count
+    @count_products = Product.count
+    @count_reviews = Review.count
   end
 
 end
